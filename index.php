ba28a766696d5f9de918da14191a6136a15e202e
rrezoni <rezon.hazrolli@hotmail.com>
2019-05-14 11:28:47 +0200

    koment

--- index.php
+++ index.php
@@ -45,8 +45,52 @@
 			<link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 			<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
 		</head>
-	<body>
+	<body id="page-top" class="index>
+		<nav class="navbar navbar-default navbar-fixed-top">
+		    <div class="container">
+		        
+		        <div class="navbar-header page-scroll">
+		            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		                <span class="sr-only">Toggle navigation</span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		            </button>
+		            <a class="navbar-brand page-scroll" href="#page-top">Bon Voyage</a>
+		        </div>
 
+		        
+		        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		            <ul class="nav navbar-nav navbar-right">
+		                <li class="hidden">
+		                    <a href="#page-top"></a>
+		                </li>
+		                <li>
+		                    <a class="page-scroll" href="#services">Services</a>
+		                </li>
+		                <li>
+		                    <a class="page-scroll" href="#portfolio">Portfolio</a>
+		                </li>
+		                <li>
+		                    <a class="page-scroll" href="#about">About</a>
+		                </li>
+		                <li>
+		                    <a class="page-scroll" href="#contact">Contact</a>
+		                </li>
+		            </ul>
+		        </div>
+		    </div>
+		</nav>
+
+		<header>
+		    <div class="container">
+		        <div class="intro-text">
+		            <div class="intro-lead-in"></div>
+		            <div class="intro-heading"></div>
+		            <a href="#services" class="page-scroll btn btn-xl">Tell Me More</a>
+		        </div>
+		    </div>
+		</header>
 	</body>
 	</html>
 ?>
