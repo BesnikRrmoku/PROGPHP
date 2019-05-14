718a8f95d61fc097bc5f2df3a2981a11926b243d
AlbinBerisha <albin199915@gmail.com>
2019-05-14 11:06:21 +0200

    start up

--- index.php
+++ index.php
@@ -1,14 +1,52 @@
-
 <!DOCTYPE html>
-
 <?php 
-
-	<html>
+	<html lang="en">
 		<head>
-			<title></title>
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+			<script> 
+			$(document).ready(function(){
+			    $("#flip").click(function(){
+			        $("#panel").slideToggle("slow");
+			    });
+			});
+			</script>
+			<style> 
+			#panel, #flip {
+			    padding: 5px;
+			    text-align: center;
+			    background-color: #e5eecc;
+			    border: solid 1px #c3c3c3;
+			}
+
+			#panel {
+			    padding: 50px;
+			    display: none;
+			}
+
+			</style>
+			<meta charset="utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<meta name="description" content="">
+			<meta name="author" content="">
+
+			<title>Bon Voyage</title>
+
+			
+			<link href="css/file1.css" rel="stylesheet">
+
+			
+			<link href="css/agency.css" rel="stylesheet">
+
+			
+			<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+			<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+			<link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+			<link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+			<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
 		</head>
 	<body>
 
 	</body>
 	</html>
- ?>
+?>
